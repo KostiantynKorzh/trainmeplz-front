e3b81ae5ef32b9e8fb152048684e117eb363daec
Kostiantyn Korzh <kostiantyn_korzh@epam.com>
2021-09-24 16:06:39 +0300

    Add animated background to home (main) component

--- src/components/pages/main/Main.tsx
+++ src/components/pages/main/Main.tsx
@@ -1,7 +1,36 @@
+import ParticlesBg from "particles-bg";
 import React from "react";
+import { Card } from "antd";
 
 const Main = () => {
-  return <>Main</>;
+  const welcomeText = `
+    Welcome to my machine learning project\n
+    Let me show how you can help training model with images  
+    `
+    .split("\n")
+    .map((str) => <p>{str}</p>);
+
+  return (
+    <>
+      <div className="center">
+        <Card
+          style={{
+            border: "solid red 1px",
+            borderRadius: "30px",
+            minHeight: "20vh",
+            maxWidth: "60vw",
+          }}
+          bordered={false}
+          bodyStyle={{ fontSize: "2.4em" }}
+        >
+          {welcomeText}
+        </Card>
+      </div>
+      {/*<div style={{ opacity: 0.4 }}>*/}
+      <ParticlesBg type="cobweb" bg={true} />
+      {/*</div>*/}
+    </>
+  );
 };
 
 export default Main;
